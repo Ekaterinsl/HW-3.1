c6c10af891068cba0f97cd5bba6037648017c7da
Apelsin <Apelsin@DESKTOP-I9KKGKE>
2022-07-02 15:32:10 +0400

    Добавьте файлы проекта.

---
+++ задание 3.1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace задание_3._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N;
+
+            Console.WriteLine("Введите число :  ");
+            N = int.Parse (Console.ReadLine());
+                        
+
+            if (N % 2==1)
+            {
+                Console.WriteLine("Число нечетное");
+            }
+            else
+            { 
+                Console.WriteLine("Число четное");
+            }
+            Console.ReadKey();
+        }
+    }
+}
